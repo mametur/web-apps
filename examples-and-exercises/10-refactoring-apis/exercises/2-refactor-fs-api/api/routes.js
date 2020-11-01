a16783e13086f4a4d5a5440c36795677a608e9d8
Mamé <mameazad.turgut@gmail.com>
2020-11-01 10:55:11 +0100

    refactorrouters exercises

--- examples-and-exercises/10-refactoring-apis/exercises/2-refactor-fs-api/api/routes.js
+++ examples-and-exercises/10-refactoring-apis/exercises/2-refactor-fs-api/api/routes.js
@@ -2,19 +2,22 @@ const handlers = require('./handlers.js');
 const express = require('express');
 
 // create the router
-_;
+router = express.Router();
 
 router.use((req, res, next) => {
-  console.log('routes!');
-  next();
+	console.log('routes!');
+	next();
 });
 
 router.get('/', (req, res) => {
-  res.send('routes!');
+	res.send('routes!');
 });
 
 // write the routes!
+router.post('/param/:value', handlers.paramValue);
 
+router.post('/query', handlers.queryValue);
 
+router.post('/body', handlers.bodyValue);
 
 module.exports = router;
